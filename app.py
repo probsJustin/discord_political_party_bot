ba0e6ce60253b609f1c0ba5e14ad3b2372b27341
Justin Hagerty <wheelsmanx@gmail.com>
2022-02-21 12:54:22 -0500

    updated some of the commands for debug

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class politics:
     senate_file = f'{default_directory}/senate.json'
     president_file = f'{default_directory}/president.json'
     configure_file = f'{default_directory}/configure.json'
+    server_voting_file = f'{default_directory}/server_voting.json'
     server_political_system_file = f'{default_directory}/server_political_systems.json'
 
 
@@ -62,12 +63,104 @@ async def getPoliticalStructure(server_id):
     return returnObject
 
 
+async def showOpenVotes(server_id):
+    returnObject = ""
+    votesReader = open(politics.server_voting_file)
+    vote_structure = json.load(votesReader)[server_id]
+    returnObject += f'Server Name: {server_id}\n'
+    for x in vote_structure:
+        returnObject += f'Vote: {x}\n'
+        returnObject += f'Time Left: {vote_structure[x]["end_date"]}\n'
+    return returnObject
+
+async def showPoliticalPosition(position_type, server_id):
+    returnObject = ""
+    political_structure = dict()
+
+    congressReader = open(politics.congress_file)
+    senateReader = open(politics.senate_file)
+    presidentReader = open(politics.president_file)
+
+    political_structure["congress"] = json.load(congressReader)[server_id]
+    political_structure["senate"] = json.load(senateReader)[server_id]
+    political_structure["president"] = json.load(presidentReader)[server_id]
+
+    if(position_type == "president"):
+        returnObject += f'[{position_type}]:\n'
+        returnObject += f'{ political_structure[position_type]["president_name"] }'
+    else:
+        returnObject += f'[{position_type}]:\n'
+        returnObject += f'{ await getMembersNames(political_structure[position_type]["members"]) }'
+
+    return returnObject
+
+
+def startVote(server_id):
+    return True
+
+
+def endVote(vote_id, server_id):
+    return True
+
+
+def appointment(user_id, server_id):
+    return True
+
+
+def removeAppointment(user_id, server_id):
+    return True
+
+def castVote(vote_id, user_id, server_id):
+    return True
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$pol'):
-        await message.channel.send(await getPoliticalStructure('server-id'))
+        content = message.content.split(' ')
+
+        if(content[1] == "show"):
+            if(content[2] == "structure"):
+                await message.channel.send(await getPoliticalStructure('server-id'))
+            if(content[2] == "votes"):
+                await message.channel.send(await showOpenVotes('server-id'))
+            if(content[2] == "congress"):
+                await message.channel.send(await showPoliticalPosition(content[2], 'server-id'))
+            if(content[2] == "senate"):
+                await message.channel.send(await showPoliticalPosition(content[2], 'server-id'))
+            if(content[2] == "president"):
+                await message.channel.send(await showPoliticalPosition(content[2], 'server-id'))
+
+        if(content[1] == "debug"):
+            if(content[2] == "start"):
+                if(content[3] == "vote"):
+                    await message.channel.send(await startVote(content[4], 'server-id'))
+
+            if(content[2] == "end"):
+                if(content[3] == "vote"):
+                    await message.channel.send(await endVote(content[4], 'server-id'))
+
+            if(content[2] == "appoint"):
+                if(content[3] == "congress"):
+                    await message.channel.send(await appointment(content[4], 'server-id'))
+                if (content[3] == "senate"):
+                    await message.channel.send(await appointment(content[4], 'server-id'))
+                if (content[3] == "president"):
+                    await message.channel.send(await appointment(content[4], 'server-id'))
+
+            if(content[2] == "remove"):
+                if(content[3] == "congress"):
+                    await message.channel.send(await removeAppointment(content[4], 'server-id'))
+                if (content[3] == "senate"):
+                    await message.channel.send(await removeAppointment(content[4], 'server-id'))
+                if (content[3] == "president"):
+                    await message.channel.send(await removeAppointment(content[4], 'server-id'))
+
+            if(content[2] == "vote"):
+                await message.channel.send(await castVote(content[3], 'fake user', 'server-id'))
+
 
 client.run('')
