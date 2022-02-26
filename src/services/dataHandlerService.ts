23d74849f2eb87e24d7f467284b040258fbb6c54
Justin Hagerty <wheelsmanx@gmail.com>
2022-02-26 08:42:54 -0500

    added some structures to the data and added a class to handle storing that data

---
+++ src/services/dataHandlerService.ts
@@ -0,0 +1,15 @@
+
+
+
+export default class dataHandlerService {
+
+    private readonly localDataStructure = "";
+    private readonly remoteDatabase = "";
+
+    constructor(){
+    }
+
+    async setDataOrigin(){
+    }
+
+}
